927dd0deafa83cbf18ce90cebaca2cc7f52f97c7
BenslimaneNassim <nassim.benslimane@etu.univ-amu.fr>
2025-06-22 18:06:23 +0200

    updating signals to capture google social account tokens

--- user/signals.py
+++ user/signals.py
@@ -14,23 +14,17 @@ def save_name(sender, instance, *args, **kwargs):
 
 
 from allauth.socialaccount.models import SocialToken
-from allauth.socialaccount.signals import social_account_added, social_account_updated
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from datetime import datetime
-import pytz
+from .models import User
 
-@receiver([social_account_added, social_account_updated])
-def get_google_tokens(request, sociallogin, **kwargs):
-    print("Signal triggered for social account added or updated.\n\n\n")
-    if sociallogin.account.provider == 'google':
-        token = SocialToken.objects.filter(account=sociallogin.account).first()
-        if token:
-            user = sociallogin.user
-            user.access_token = token.token
-            print(f"Access token: {user.access_token}")
-            user.refresh_token = token.token_secret  # Vérifie si présent
-            print(f"Refresh token: {user.refresh_token}")
-            if token.expires_at:
-                user.token_expiry = token.expires_at  # déjà un datetime avec timezone
-                print(f"Token expires at: {user.token_expiry}")
-            user.save()
+@receiver(post_save, sender=SocialToken)
+def save_user_tokens(sender, instance, created, **kwargs):
+    account = instance.account
+    if account.provider == 'google':
+        user = account.user
+        user.access_token = instance.token
+        user.refresh_token = instance.token_secret or ''
+        user.token_expiry = instance.expires_at
+        print("Token saved for user:", user.email)
+        user.save()
